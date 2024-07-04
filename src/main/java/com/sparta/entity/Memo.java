499abed9fb820e220857765bb97c7ce6fa0ba15f
1seyoung <20171631@edu.hanbat.ac.kr>
2024-07-04 10:02:08 +0900

    entity 이해하기

--- src/main/java/com/sparta/entity/Memo.java
+++ src/main/java/com/sparta/entity/Memo.java
@@ -1,14 +1,13 @@
 package com.sparta.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity // JPA가 관리할 수 있는 Entity 클래스 지정
+
 @Table(name = "memo") // 매핑할 테이블의 이름을 지정
 public class Memo {
     @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     // nullable: null 허용 여부
@@ -19,4 +18,28 @@ public class Memo {
     // length: 컬럼 길이 지정
     @Column(name = "contents", nullable = false, length = 500)
     private String contents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
 }
